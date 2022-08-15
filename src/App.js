a17b529303c22638e17f2f446ffec3c66279b303
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2022-08-14 21:55:07 -0500

    app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import { Container } from "@mui/system";
 import "./App.css";
 import { Barra } from "./components/Barra";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Barra />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Barra/>
+    </Container>
   );
 }
 
