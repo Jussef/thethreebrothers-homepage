5841343a5a0338d5d3568a28ed6bec4019f55248
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2022-08-17 16:48:16 -0500

    libreria scrol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Container } from "@mui/system";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Container } from "@mui/system";
 import { Barra } from "./components/Barra";
+import { Timeline } from "./components/Timeline";
 
 function App() {
   return (
-    <Container>
-      <Barra/>
-    </Container>
+    <Router>
+      <div className="container">
+        <Barra />
+        <Container>
+          <Timeline />
+        </Container>
+      </div>
+    </Router>
   );
 }
 
