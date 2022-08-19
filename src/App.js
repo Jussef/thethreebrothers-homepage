74cc75756b25caa952dd65e25d3c502dde42f026
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2022-08-18 22:31:58 -0500

    inti

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Container } from "@mui/system";
 import { Barra } from "./components/Barra";
 import { Timeline } from "./components/Timeline";
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Barra />
-        <Container>
-          <Timeline />
-        </Container>
-      </div>
+      <Barra />
+      <Timeline />
     </Router>
   );
 }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Box from "@mui/material/Box";
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
 import "./Timeline.css";
-import img1 from '../img/img01.jpg'
+import img1 from "../img/img01.jpg";
 
 const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
 const FadeUp = batch(Fade(), Move(), Sticky());
@@ -10,42 +10,39 @@ const FadeUp = batch(Fade(), Move(), Sticky());
 export class Timeline extends Component {
   render() {
     return (
-      <Box sx={{ flexGrow: 1 }}>
-        <ScrollContainer>
-          <ScrollPage>
-            <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
-              <span style={{ fontSize: "30px" }}>Prueba haciendo scroll 😀 🔻</span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={ZoomInScrollOut}>
-              {/* <span style={{ fontSize: "40px" }}>I'm FadeUpScrollOut ✨</span> */}
-              <img src={img1} alt="1" />
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <span style={{ fontSize: "40px" }}>I'm FadeUp ⛅️</span>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
-              <span style={{ fontSize: "40px" }}>
-                <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
-                <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>- I'm Dante Chun -<Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
-                <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
-              </span>
-            </div>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={batch(Fade(), Sticky())}>
-              <span style={{ fontSize: "40px" }}>Done</span>
-              <br />
-              <span style={{ fontSize: "30px" }}>There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation</span>
-            </Animator>
-          </ScrollPage>
-        </ScrollContainer>
-      </Box>
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+            <span style={{ fontSize: "30px" }}>Prueba haciendo scroll 😀 🔻</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <Animator animation={batch(Fade(), ZoomInScrollOut)}>
+            <span style={{ fontSize: "40px" }}>La Historia empieza en 1997</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={2}>
+          <Animator animation={FadeUp}>
+            <img id="imagen1" src={img1} alt="1" />
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={3}>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+            <span style={{ fontSize: "40px" }}>
+              <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
+              <Animator animation={MoveIn(1000, 0)}>Nice to meet you 🙋🏻‍♀️</Animator>- I'm Dante Chun -<Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+              <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+            </span>
+          </div>
+        </ScrollPage>
+        <ScrollPage page={4}>
+          <Animator animation={batch(Fade(), Sticky())}>
+            <span style={{ fontSize: "40px" }}>Done</span>
+            <br />
+            <span style={{ fontSize: "30px" }}>There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation</span>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
     );
   }
 }
